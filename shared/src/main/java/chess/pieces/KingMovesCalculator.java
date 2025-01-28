aee7d0f28f81d82373564b0e325f5090b0c39547
Blake Doty <blaked609@gmail.com>
2025-01-28 02:06:39 -0700

    passed king tests

--- shared/src/main/java/chess/pieces/KingMovesCalculator.java
+++ shared/src/main/java/chess/pieces/KingMovesCalculator.java
@@ -13,13 +13,34 @@ import java.util.Objects;
 public class KingMovesCalculator implements PieceMovesCalculator{
     private final ChessGame.TeamColor pieceColor;
     ArrayList<ChessMove> validMoves = new ArrayList<>();
+    private final int[][] possibleMoves = {{1,0}, {1,1}, {0,1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
     public KingMovesCalculator(ChessGame.TeamColor pieceColor) {
         this.pieceColor = pieceColor;
     }
 
     @Override
     public Collection<ChessMove> getPieceMoves(ChessBoard board, ChessPosition myPosition) {
+
+        for(int[] possibleMove : possibleMoves) {
+            int newRow = myPosition.getRow() + possibleMove[0];
+            int newCol = myPosition.getColumn() + possibleMove[1];
+            if((newRow > 8 || newCol > 8 || newRow < 1 || newCol < 1) || isStuck(newRow, newCol, board, this.pieceColor)) {
+                continue;
+            }
+            ChessPosition newPosition = new ChessPosition(newRow, newCol);
+            ChessMove newMove = new ChessMove(myPosition, newPosition, null);
+            validMoves.add(newMove);
+        }
         return validMoves;
+    }
+
+    public boolean isStuck(int newRow, int newCol, ChessBoard board, ChessGame.TeamColor pieceColor) {
+        if (((newRow <= 8) && (newRow >= 1)) && ((newCol <= 8) && (newCol >= 1))) {
+            if(board.getPiece(new ChessPosition(newRow, newCol)) != null){
+                return board.getPiece(new ChessPosition(newRow, newCol)).getTeamColor() == pieceColor;
+            }
+        }
+        return false;
     }
 
     @Override
